4e7cf66e7de34e5309ebe7360ed73548132cbf5b
cthierst <chlothier@gmail.com>
2023-04-20 10:37:08 -0600

    updated tests and added test

--- scripts/01-test_data.R
+++ scripts/01-test_data.R
@@ -5,13 +5,14 @@
 # Contact: chloe.thierstein@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Need to be able to load in simulated data created
+# Other Notes: Test are applicable to all simulated data, the tests below test 2021
 
 
 #### Workspace setup ####
 library(here)
 
 #### Load in Simulated Data ####
-simulated_gss_data <- read.csv(here::here("./inputs/simulated_gss_data.csv"))
+simulated_gss_data <- read.csv(here::here("./inputs/simulated_2021_gss_data.csv"))
 
 #### Testing Simulated Data ####
 
@@ -26,5 +27,9 @@ simulated_gss_data$"cohort" |>
 
 #### Test 3 ####
 simulated_gss_data$"marital_status" |>
+  unique()
+
+#### Test 4 ####
+simulated_gss_data$"political_viewpoint" |>
   unique()
 
